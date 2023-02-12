864c0cf4d36f128eb82489029d34d0d350367985
Adithya <adithyapantangi123@gmail.com>
2023-02-12 10:42:15 +0530

    Added tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,26 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <span>React Retro</span>
+    <div className="min-w-screen  min-h-screen bg-gradient-to-b from-blue-800 to-blue-800">
+      <span className="text-3xl text-white">React Retro</span>
+      <Card />
+      <CardNew />
+    </div>
+  );
+};
+
+const Card = () => {
+  return (
+    <div className="h-24 w-48 rounded-lg border-pink-500 bg-blue-400 text-white hover:border-pink-900">
+      <span>Retro Card</span>
+    </div>
+  );
+};
+
+const CardNew = () => {
+  return (
+    <div className="h-24 w-48 rounded-lg border-red-500 bg-blue-400 text-white hover:bg-opacity-10">
+      <span>Retro Card</span>
     </div>
   );
 };
